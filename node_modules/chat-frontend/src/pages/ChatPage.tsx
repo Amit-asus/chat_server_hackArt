faaccc7bcd044734806983d4058ef93744d5f62a
amit-mac <144879811+Amit-asus@users.noreply.github.com>
2026-04-20 22:05:00 +0530

    Delete node_modules directory

--- node_modules/chat-frontend/src/pages/ChatPage.tsx
+++
@@ -1,7 +0,0 @@
-import { useSocket } from '../hooks/useSocket';
-import MainLayout from '../components/layout/MainLayout';
-
-export default function ChatPage() {
-  useSocket(); // Initialize socket connection
-  return <MainLayout />;
-}
